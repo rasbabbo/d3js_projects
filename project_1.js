29a96e97379f96e49483b9be1fdbfc080527138c
rasbabbo <rasbabbo@yahoo.com>
2014-11-01 15:01:55 -0700

    json generated rect

--- project_1.js
+++ project_1.js
@@ -1,37 +1,90 @@
-var jsonCircles = [
-	{
-		"x_axis": 30,
-		"y_axis": 30,
-		"radius": 20,
-		"color": "green"
-	}, {
-		"x_axis": 70,
-		"y_axis": 70,
-		"radius": 20,
-		"color": "purple"
-	}, {
-		"x_axis": 110,
-		"y_axis": 100,
-		"radius": 20,
-		"color": "red"
-	}];
+var jsonRectangles = [
+	{ "x_axis" : 10, "y_axis": 10, "height": 20, "width":20, "color" : "green" },
+	{ "x_axis" : 160, "y_axis": 40, "height": 20, "width":20, "color" : "purple" },
+	{ "x_axis" : 70, "y_axis": 70, "height": 20, "width":20, "color" : "red" }
+];
 
-var spaceCircles = [30, 70, 110];
+
+//these are holders for the max coord. set dynamically by the x/y values
+var max_x = 0;
+var max_y = 0;
+
+//finds the max x/y values - remember to add the size of the object
+for (var i = 0; i < jsonRectangles.length; i++) {
+	var temp_x, temp_y;
+	var temp_x = jsonRectangles[i].x_axis + jsonRectangles[i].width;
+	var temp_y = jsonRectangles[i].y_axis + jsonRectangles[i].height;
+
+	if (temp_x >= max_x) {
+		max_x = temp_x;
+	}
+
+	if (temp_y >= max_y) {
+		max_y = temp_y;
+	}
+}
 
 var svgContainer = d3.select("body").append("svg")
-																		.attr("width", 200)
-																		.attr("height", 200);
+																		.attr("width", max_x + 20)
+																		.attr("height", max_y + 20);
 
-var circles = svgContainer.selectAll("circle")
-													.data(jsonCircles)
-													.enter()
-													.append("circle");
+var rectangles = svgContainer.selectAll("rect")
+															.data(jsonRectangles)
+															.enter()
+															.append("rect");
 
-var circleAttributes = circles
-												.attr("cx", function (d) { return d.x_axis; })
-												.attr("cy", function (d) { return d.y_axis; })
-												.attr("r", function (d) { return d.radius; })												
-												.style("fill", function (d) {return d.color; });
+var rectangleAttributes = rectangles
+													.attr("x", function(d) { return d.x_axis; })
+													.attr("y", function(d) { return d.y_axis; })
+													.attr("height", function(d) { return d.height; })
+													.attr("width", function(d) { return d.width; })
+													.style("fill", function(d) { return d.color; });
+
+
+
+
+
+
+
+
+
+
+
+//using josn to dynamicaly alter visuals
+// var jsonCircles = [
+// 	{
+// 		"x_axis": 30,
+// 		"y_axis": 30,
+// 		"radius": 20,
+// 		"color": "green"
+// 	}, {
+// 		"x_axis": 70,
+// 		"y_axis": 70,
+// 		"radius": 20,
+// 		"color": "purple"
+// 	}, {
+// 		"x_axis": 110,
+// 		"y_axis": 100,
+// 		"radius": 20,
+// 		"color": "red"
+// 	}];
+
+// var spaceCircles = [30, 70, 110];
+
+// var svgContainer = d3.select("body").append("svg")
+// 																		.attr("width", 200)
+// 																		.attr("height", 200);
+
+// var circles = svgContainer.selectAll("circle")
+// 													.data(jsonCircles)
+// 													.enter()
+// 													.append("circle");
+
+// var circleAttributes = circles
+// 												.attr("cx", function (d) { return d.x_axis; })
+// 												.attr("cy", function (d) { return d.y_axis; })
+// 												.attr("r", function (d) { return d.radius; })												
+// 												.style("fill", function (d) {return d.color; });
 
 // var circleAttributes = circles
 // 												.attr("cx", function (d) { return d; })																						
