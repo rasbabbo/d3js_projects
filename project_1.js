65d3c08f57dc1f614785333e8eba33989289eb28
rasbabbo <rasbabbo@yahoo.com>
2014-11-11 18:56:25 -0800

    debugged svg text element

--- project_1.js
+++ project_1.js
@@ -6,16 +6,29 @@ var svgContainer = d3.select("body").append("svg")
 																		.attr("width", 200)
 																		.attr("height", 200);
 
-var circles = svgContainer.selectAll("circle")
-													.data(circleData)
-													.enter()
-													.append("circle");
+var text = svgContainer.selectAll("text")
+											 .data(circleData)
+											 .enter()
+											 .append("text");
 
-var circleAttributes = circles
-											 .attr("cx", function (d) {return d.cx; })																	
-											 .attr("cy", function (d) {return d.cy; })
-											 .attr("r", function (d) {return d.radius; })
-											 .style("fill", function (d) {return d.color; });
+var textLabels = text
+								 .attr("x", function(d) {return d.cx; })
+								 .attr("y", function(d) {return d.cy; })
+								 .text( function(d) { return "( " + d.cx + ", " + d.cy + " )"; })
+								 .attr("font-family", "sans-serif")
+								 .attr("font-size", "20px")
+								 .attr("fill", "red");
+	
+// var circles = svgContainer.selectAll("circle")
+// 													.data(circleData)
+// 													.enter()
+// 													.append("circle");
+
+// var circleAttributes = circles
+// 											 .attr("cx", function (d) {return d.cx; })																	
+// 											 .attr("cy", function (d) {return d.cy; })
+// 											 .attr("r", function (d) {return d.radius; })
+// 											 .style("fill", function (d) {return d.color; });
 
 
 
